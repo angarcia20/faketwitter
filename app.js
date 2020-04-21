6318512bebb18424d1508377e0aa7aa5579325f6
angarcia20 <angarcia20@poligran.edu.co>
2020-04-21 01:17:48 -0500

    angarcia20 - 21/04/2020 - correcion back end(CORS)

--- app.js
+++ app.js
@@ -21,6 +21,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+      // allowed XHR methods  
+      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+      res.send();
+  });
+});
+
+
 //Set the routing routes to the each script
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
